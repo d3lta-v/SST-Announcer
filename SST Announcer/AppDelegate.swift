4915b54b8ad9368c5eb04d016d48cb673de07ed0
Pan Ziyue <random.rrr3@gmail.com>
2016-11-23 08:33:16 +0800

    Added remote notification entitlements and callbacks

--- SST Announcer/AppDelegate.swift
+++ SST Announcer/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import OneSignal
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        OneSignal.initWithLaunchOptions(launchOptions, appId: "76349b34-5515-4dbe-91bd-3dff5ca1e780")
         return true
     }
 
