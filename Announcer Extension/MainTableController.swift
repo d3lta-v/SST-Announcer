bec2cb0f771c876e0641a6a01226725e6e057c93
Pan Ziyue <random.rrr3@gmail.com>
2016-12-17 22:41:53 +0800

    Added proper logic for the table in the ⌚️ version

--- Announcer Extension/MainTableController.swift
+++ Announcer Extension/MainTableController.swift
@@ -9,23 +9,52 @@
 import WatchKit
 import Foundation
 
-
 class MainTableController: WKInterfaceController {
 
-    override func awake(withContext context: Any?) {
-        super.awake(withContext: context)
-        
-        // Configure interface objects here.
+  fileprivate let feeder = Feeder()
+
+  @IBOutlet weak var table: WKInterfaceTable!
+
+  override func awake(withContext context: Any?) {
+    super.awake(withContext: context)
+
+    // Configure interface objects here.
+    feeder.delegate = self
+    feeder.requestFeedsAsynchronous()
+  }
+
+  override func willActivate() {
+    // This method is called when watch view controller is about to be visible to user
+    super.willActivate()
+  }
+
+  override func didDeactivate() {
+    // This method is called when watch view controller is no longer visible
+    super.didDeactivate()
+  }
+
+}
+
+extension MainTableController: FeederDelegate {
+
+  func feedFinishedParsing(withFeedArray feedArray: [FeedItem]?, error: Error?) {
+    DispatchQueue.main.async {
+      self.table.setNumberOfRows(self.feeder.feeds.count, withRowType: "postRow")
+      for i in 0..<self.feeder.feeds.count {
+        guard let controller = self.table.rowController(at: i) as? PostRowController else {
+          //TODO: Relay telemetry, severe error may have occured
+          continue
+        }
+        controller.feed = self.feeder.feeds[i]
+      }
     }
-    
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-    }
-    
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
-    }
+  }
+
+  func feedLoadedFromCache() {
+  }
+
+  func feedLoadedPercent(_ percent: Float) {
+    print("Loaded percent: \(percent * 100)")
+  }
 
 }
---
+++ Announcer Extension/PostRowController.swift
@@ -0,0 +1,28 @@
+//
+//  PostRowController.swift
+//  SST Announcer
+//
+//  Created by Pan Ziyue on 17/12/16.
+//  Copyright © 2016 FourierIndustries. All rights reserved.
+//
+
+import WatchKit
+
+class PostRowController: NSObject {
+
+  @IBOutlet var titleLabel: WKInterfaceLabel!
+  @IBOutlet var dateLabel: WKInterfaceLabel!
+  @IBOutlet var authorLabel: WKInterfaceLabel!
+
+  var feed: FeedItem? {
+    didSet {
+      guard let feed = feed else {
+        return
+      }
+      titleLabel.setText(feed.title)
+      dateLabel.setText(feed.date.decodeToTimeAgo())
+      authorLabel.setText(feed.author)
+    }
+  }
+
+}
